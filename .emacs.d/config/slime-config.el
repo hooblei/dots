75e514b71180587b045dae251dc968d0d62f6a57
maresp <me@cubeball.net>
2015-12-09 10:33:50 +0100

    resolving local change

--- .emacs.d/config/slime-config.el
+++ .emacs.d/config/slime-config.el
@@ -12,9 +12,4 @@
 (evil-define-key 'normal emacs-lisp-mode-map (kbd "K")
   'elisp-slime-nav-describe-elisp-thing-at-point)
 
-;(progn
-;  (require 'elisp-slime-nav)
-;  (defun my-lisp-hook ()
-;    (elisp-slime-nav-mode)
-;    (turn-on-eldoc-mode))
-;  (add-hook 'emacs-lisp-mode-hook 'my-lisp-hook))
+(provide 'slime-config)
